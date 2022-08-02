ae4bccd6807434538042218e29c3c9ce25c3c6ba
John Edward Gonzalez <edscodes@gmail.com>
2022-08-01 22:55:42 -0500

    Add files via upload

--- platzi/Curso basico de prog/piedra_papel_tij.js
+++ platzi/Curso basico de prog/piedra_papel_tij.js
@@ -1,7 +1,9 @@
+//obtener numero aleatorio para Pc
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+//asignar una jugada a un numero
 function eleccion(jugada) {
     let resultado = ""
     if(jugada == 1){
@@ -16,23 +18,31 @@ function eleccion(jugada) {
     return resultado
 }
 
-let jugador = parseInt(prompt("por favor elige: 1 para piedra, 2 para papel, 3 para tijera"))
+let jugador = 0
 let min = 1
 let max = 3
-let pc = aleatorio (1,3)
+let pc = 0
+let triunfos = 0
+let perdidas = 0
 
-alert("tu eliges:" + eleccion(jugador))
-alert("PC elige:" + eleccion(pc))
+while(triunfos < 3 && perdidas < 3){
+    pc = aleatorio (1,3)
+    jugador = parseInt(prompt("por favor elige: 1 para piedra, 2 para papel, 3 para tijera"))
+    alert("tu eliges:" + eleccion(jugador))
+    alert("PC elige:" + eleccion(pc))
 
-//combate
-if(pc == jugador){
-    alert("EMPATE")
-}else if(jugador == 1 && pc == 3){
-    alert("GANASTE")
-}else if(jugador == 2 && pc == 1){
-    alert("GANASTE")
-}else if(jugador == 3 && pc == 2){
-    alert("GANASTE")
-}else{
-    alert("PERDISTE")
+    // logica del combate
+if(pc == jugador) {
+        alert("EMPATE")
+    } else if((jugador == 1 && pc == 3) || (jugador == 2 && pc ==1) || (jugador ==3& pc ==2)) {
+        triunfos++
+        alert("GANASTE")
+    } else {
+        perdidas++
+        alert("PERDISTE")
 }
+}
+alert("Ganaste " + triunfos + " veces y perdiste " + perdidas + " veces.")
+if(perdidas >= 3){
+    alert("La pc es la ganadora")
+}else(alert("Felicidades, eres el ganador"))
